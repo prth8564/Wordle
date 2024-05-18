b297013bd5203822b55d5f45ec9b96d5989a6976
prth8564 <preetham10107@gmail.com>
2024-05-18 09:22:45 +0530

    submitting guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -5,11 +5,11 @@ const useWordle = () => {
     const [turn , setTurn] = useState(0);
     const [currentGuess , setCurrentGuess]  = useState('');
     const [guess , setGuess] = useState('');
-    const [history , setHistory] = useState('');
+    const [history , setHistory] = useState(['apple' , 'fruit']);
     const [isCorrect , setIsCorrect] = useState(false);
 
     const formatGuess = () => {
-
+        console.log("Formattting now----------", currentGuess);
     }
 
     const addNewGuess = () => {
@@ -18,6 +18,25 @@ const useWordle = () => {
 
     const handleKeyUp = ({key}) => {
         
+
+        if(key === 'Enter'){
+             if(turn > 5){
+                console.log("No more than 5 chances")
+                return;
+            }
+            if(history.includes(currentGuess)){
+                console.log("You have alreday used that word");
+                return;
+            }
+            if(currentGuess.length < 5){
+                console.log("word has to be 5 characters");
+                return;
+            }
+            formatGuess();
+        }
+
+
+
         if(key === 'Backspace'){
             setCurrentGuess((prev) => {
                 return prev.slice(0 , -1);
